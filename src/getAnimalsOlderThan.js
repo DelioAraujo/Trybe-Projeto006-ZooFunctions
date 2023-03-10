6d1355c7b1e144ff48ecb194c8ab4ec3ccfff687
DelioAraujo <delioaraujo@gmail.com>
2023-03-10 17:05:33 -0300

    item 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
   // seu código aqui
+  const especie = data.species.find((elemento) => elemento.name === animal);
+  const idadecheck = especie.residents.every((elemento) => elemento.age >= age);
+  return idadecheck;
 };
+
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
   // seu código aqui
+  if(employeeName.length === 0){return {}}
+  else {
+    
+  }
+
 };
 
 module.exports = getEmployeeByName;
