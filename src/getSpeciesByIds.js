d2e8e633f5670cc6b3298633666e47b4c1a66ed3
DelioAraujo <delioaraujo@gmail.com>
2023-03-10 09:58:34 -0300

    item 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === 0) {
+    return [];
+  }
+
+  if (ids.length === 1) {
+    const id = ids[0];
+    return [data.species.find((elemento) => elemento.id === id)];
+  }
+
+  return data.species.filter((elemento) => ids.includes(elemento.id));
 };
 
 module.exports = getSpeciesByIds;
