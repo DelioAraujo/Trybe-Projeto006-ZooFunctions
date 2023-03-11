52e8e3b3dc769ab9e3bab708a1ddc87ad6eeb702
DelioAraujo <delioaraujo@gmail.com>
2023-03-11 11:17:33 -0300

    item 7

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+ 
+  // faz um novo array de objetos apenas com a informação "name: popularity".
+  const contagemTotal = data.species.map((elemento2) => { return `${elemento2.name}: ${elemento2.popularity}`})
+  // separa o objeto que tem o name igual a specie do objeto colocado no argumento
+  const bicho = data.species.find((elemento) => elemento.name === animal.species);
+  // de acordo com o objeto achado acima, guarda a informação do popularity dele
+  const contagemIndividual = bicho.popularity
+  // faz um array com todos animais que tem sex igual ao sex do argumento
+  const genero = data.species.residents.filter((elemento) => elemento.sex === animal.sex)
+  // conta quantos objetos tem dentro do array genero
+  const contagemGenero = genero.length
+
+  if(!animal){
+    return contagemTotal
+  } else if (animal.length === 1){
+    return contagemIndividual
+  } else if (animal.length ===2){
+    return contagemGenero
+  }
+
 };
 
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,23 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+    it ('retorna a quantidade de elefantes se o argumento for count', () => {
+        expect(handlerElephants('count')).toBe(4);
+    })
+    it ('retorna um array com a relação dos nomes de todos os elefantes se o argumento for names', () => {
+        expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+    })
+    it ('retorna a média de idade dos elefantes se o argumento for averageAge', () => {
+        expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+    })
+    it ('retorna a localização dos elefantes dentro do Zoológico se o argumento for location', () => {
+        expect(handlerElephants('location')).toBe('NW');
+    })
+    it ('retorna a popularidade dos elefantes se o argumento for popularity', () => {
+        expect(handlerElephants('popularity')).toBe(5);
+    })
+    it ('retorna um array com a relação de dias em que é possível visitar os elefantes se o argumento for availability', () => {
+        expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+    })
+});
+
