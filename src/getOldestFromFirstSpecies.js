fef5b0b11b4719af6cacd21947542aed4c6c4f28
DelioAraujo <delioaraujo@gmail.com>
2023-03-11 17:33:53 -0300

    item 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const colaborador = data.employees.find((elemento) => elemento.id === id);
+  const primeiraEspecieId = colaborador.responsibleFor[0];
+  const primeiraEspecie = data.species.find((elemento) => elemento.id === primeiraEspecieId);
+  const animalMaisVelho = primeiraEspecie.residents.reduce((acc, curr) => {
+    return acc.age > curr.age ? acc : curr;
+  });
+
+  return [animalMaisVelho.name, animalMaisVelho.sex, animalMaisVelho.age];
 };
 
 module.exports = getOldestFromFirstSpecies;
