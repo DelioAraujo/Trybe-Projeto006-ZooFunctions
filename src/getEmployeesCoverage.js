0ee8ea468a4279b8cf638d22d2dd814c2696c10c
DelioAraujo <delioaraujo@gmail.com>
2023-03-12 12:30:36 -0300

    13

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,68 +2,31 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getEmployeesCoverage = (objeto) => {
-  // seu código aqui
-  if (objeto.name){
-    const empregado = data.employees.find((elemento) =>{ elemento.name === objeto.name || elemento.lastName === objeto.name});
-    const identidade = empregado.id;
-    const  nomeCompleto = `${empregado.name} ${empregado.lastName}`;
-    const animaisQueCuida = empregado.responsibleFor.filter((element) => element === species.id );
-    const nomeAnimaisQueCuida = animaisQueCuida.map((element) => element.name )
-    const locaisAnimais = animaisQueCuida.map((elements) => elements.location )
+  // if (objeto.name) {
+  //   const empregado = data.employees.find((element) => {
+  //     return element.firstName === objeto.name || element.lastName === objeto.name
+  //   })
 
-    return {
-      'id': identidade,
-      'fullname': nomeCompleto,
-      'species': nomeAnimaisQueCuida,
-      'locations': locaisAnimais
-    }
-    } else if ( objeto.id){
-      const empregado = data.employees.find((elemento) =>{ elemento.id === objeto.id});
-      const identidade = empregado.id;
-      const  nomeCompleto = `${empregado.name} ${empregado.lastName}`;
-      const animaisQueCuida = empregado.responsibleFor.filter((element) => element === species.id );
-      const nomeAnimaisQueCuida = animaisQueCuida.map((element) => element.name )
-      const locaisAnimais = animaisQueCuida.map((elements) => elements.location )
+  //   const responsabilidade = empregado.responsibleFor.map((element) => {
+  //     return species.find((element2) => {element2.id === element})
+  //   })
 
-      return {
-        'id': identidade,
-        'fullname': nomeCompleto,
-        'species': nomeAnimaisQueCuida,
-        'locations': locaisAnimais
-      }
-    } else if (!objeto){
+  //   const nomesResponsabilidades = responsabilidade.map((element3) => {
+  //     return element3.name})
 
-        data.employees.map((element) => {
-        
-          return {
-            'id': element.id,
-            'fullname': `${element.name} ${element.lastName}`,
-            'species': listaAnimais,
-            'location':
+  //   const locais = responsabilidade.map ((element4) => {
+  //     return element4.location})
+
+  //   const info = {}
+  //   info["id"] = empregado.id;
+  //   info["fullname"] = `${empregado.firstName} ${empregado.lastName}`;
+  //   info['species'] = nomesResponsabilidades
+  //   info['locations'] = locais
+
+  //   return info
+  // }
 
 
-
-
-
-
-
-
-
-
-        })
-
-
-      }
-
-
-
-
-
-
-
-
-
-
-
+}
 
 module.exports = getEmployeesCoverage;
