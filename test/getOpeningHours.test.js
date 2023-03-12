a81952a8e1dfa3958c059d9a0fa42aa6662d9a3e
DelioAraujo <delioaraujo@gmail.com>
2023-03-12 12:50:02 -0300

    13

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -10,7 +10,7 @@ describe('Testes da função getOpeningHours', () => {
       Saturday: { open: 8, close: 10 },
       Sunday: { open: 8, close: 8 },
       Monday: { open: 0, close: 0 },
-   });
+    });
   });
 
   it('Para os argumentos Monday e 09:00-AM deve retornar a string The zoo is closed', () => {
@@ -18,11 +18,11 @@ describe('Testes da função getOpeningHours', () => {
   });
 
   it('Para os argumentos Tuesday e 09:00-AM deve retornar a string The zoo is open', () => {
-    expect(getOpeningHours('Tuesday', '09:00-AM' )).toBe('The zoo is open');
- });
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
 
   it('Para os argumentos Wednesday e 09:00-PM deve retornar a string The zoo is closed', () => {
-    expect(getOpeningHours('Wednesday', '09:00-PM' )).toBe('The zoo is closed');
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
   });
 
   it('Para os argumentos Thu e 09:00-AM deve lançar uma exceção com a mensagem: "The day must be valid. Example: Monday"', () => {
@@ -34,13 +34,13 @@ describe('Testes da função getOpeningHours', () => {
   it('Para os argumentos Friday e 09:00-ZM deve lançar uma exceção com a mensagem: "The abbreviation must be \'AM\' or \'PM\'"', () => {
     expect(() => {
       getOpeningHours('Friday', '09:00-ZM');
-    }).toThrow("The abbreviation must be 'AM' or 'PM'");
- });
+    }).toThrow('The abbreviation must be AM or PM');
+  });
 
   it('Para os argumentos Saturday e C9:00-AM deve lançar uma exceção com a mensagem: "The hour should represent a number"', () => {
     expect(() => {
       getOpeningHours('Saturday', 'C9:00-AM');
-   }).toThrow('The hour should represent a number');
+    }).toThrow('The hour should represent a number');
   });
 
   it('Para os argumentos Sunday e 09:c0-AM deve lançar uma exceção com a mensagem: "The minutes should represent a number"', () => {
