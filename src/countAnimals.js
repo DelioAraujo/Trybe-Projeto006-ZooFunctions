7ee192064fda63004ba4139c33c2cbe1c87ff70c
DelioAraujo <delioaraujo@gmail.com>
2023-03-12 10:08:27 -0300

    item 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,29 +1,21 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
   if (!animal) {
     const listaCompleta = data.species.reduce((acc, crr) => {
       acc[crr.name] = crr.residents.length;
       return acc;
     }, {});
-
     return listaCompleta;
   }
-
   if (animal.species) {
     const bicho = data.species.find((element) => element.name === animal.species);
-    const quantiadeBicho = bicho.residents.length;
-
-    return quantiadeBicho;
+    return bicho.residents.length;
   }
-
   if (animal.sex) {
     const bicho = data.species.find((element) => element.name === animal.species);
     const bichoPorSexo = bicho.residents.filter((element2) => element2.sex === animal.sex);
-    const quantidadePorSexo = bichoPorSexo.length;
-
-    return quantidadePorSexo;
+    return bichoPorSexo.length;
   }
 };
 
